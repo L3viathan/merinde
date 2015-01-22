bf9bc2e7ca1612c20ae0e5955c9f5e5c6b80973d
L3viathan <git@l3vi.de>
2015-01-23 00:26:46 +0100

    main script plan

--- merinde.py
+++ merinde.py
@@ -10,3 +10,10 @@ def htmlFromFile(filename):
     '''Reads file and converts markdown to HTML'''
     file_handle = open(filename)
     return markdown.markdown(file_handle.read(),output_format = config["output_format"])
+
+
+# Things that should happen in here:
+
+# 1. Get list of files that have changed / been created
+# 2. Read them in and compile to HTML. Insert into template and do replacements.
+# 3. Recompile index files. Get order by creation time of md files
