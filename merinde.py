d99f2d6c50850809ad5c50dbe5922611304ad86d
L3viathan <git@l3vi.de>
2015-01-22 23:39:47 +0100

    config file, html conversion wrapper

--- merinde.py
+++ merinde.py
@@ -3,3 +3,10 @@
 '''
 Python CMS compiling from Markdown to static HTML
 '''
+import markdown
+from config import config
+
+def htmlFromFile(filename):
+    '''Reads file and converts markdown to HTML'''
+    file_handle = open(filename)
+    return markdown.markdown(file_handle.read(),output_format = config["output_format"])
