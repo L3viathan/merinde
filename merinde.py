76152a09402dcf80b19ed61d91e6249b8b600867
L3viathan <git@l3vi.de>
2015-01-23 01:18:03 +0100

    skeleton code in merinde.py

--- merinde.py
+++ merinde.py
@@ -4,6 +4,8 @@
 Python CMS compiling from Markdown to static HTML
 '''
 import markdown
+import glob
+
 from config import config
 
 def htmlFromFile(filename):
@@ -15,5 +17,20 @@ def htmlFromFile(filename):
 # Things that should happen in here:
 
 # 1. Get list of files that have changed / been created
+compile_agenda = []
+for filename in glob.glob("posts/*.md"):
+    # read last changed time and compare to some time saved somewhere else (?)
+    # if changed (or new), add to compile_agenda
+    pass
 # 2. Read them in and compile to HTML. Insert into template and do replacements.
+for filename in compile_agenda:
+    html = htmlFromFile(file)
+    # now apply template things: use loaded template and insert contents from generated html
+    # then write to file
+
 # 3. Recompile index files. Get order by creation time of md files
+if compile_agenda: #list isn't empty, so something has changed
+    # get all posts, in order
+    # remove index file(s)
+    # load index template, fill with contents
+    pass
