ab2f595f1ef08fb7d9473cfc30ffe3e5519b743f
L3viathan <git@l3vi.de>
2015-01-26 16:07:55 +0100

    logo and name draft. plus more actual reviews

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 config = {
     "output": "html5",
     "template": "default",
-    "site_name": "Test Foo Bar",
-    "pagination": 4, # split index after n posts
+    "site_name": "Isocaproic",
+    "pagination": 2, # split index after n posts
     "max_stars": 5
 }
 
